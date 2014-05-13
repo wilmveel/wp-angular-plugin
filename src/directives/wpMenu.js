e62751ec633871e4b2c61fb183cd0d457e4c74af
Willem Veelenturf <willem.veelenturf@capgemini.com>
2014-05-13 21:14:02 +0530

    Fix for menu directive

--- src/directives/wpMenu.js
+++ src/directives/wpMenu.js
@@ -1,32 +1,42 @@
 angular.module('wpAngular').directive("wpMenu", function($log, $http, wpBloginfo){
 	return {
-		transclude: 'element',
-    	scope: {},
+		transclude: true,
+    	
 		link: function($scope, $element, $attr, ctrl, $transclude){
 			
+			$scope.items = null;
+
 			if($attr.name){
 				$http.get(wpBloginfo.baseUrl + "/wp-json/menus/" + $attr.name).success(function(data){
 					$log.debug("Data", data);
 					$scope.items = data;		
 				});
 			}else{
-				$scope.items = $scope.$parent.childeren;
+				$scope.items = $scope.childeren;
 			}
 
 			$scope.$watchCollection("items", function(collection) {
-							
-				angular.forEach(collection, function(item){
-					
-					//Create new scope for every new item and load attributes to the scope
-					var childScope = $scope.$new();
-					angular.forEach(item, function(value, key){
-						childScope[key] = value;
-					});
+				
+				$log.debug("collection", collection);
+				$log.debug("collection", $scope.items);
 
-		      		$transclude(childScope, function(clone) {
-		      			$element.parent().append(clone);
-		      		});
-		      	});
+				if(collection === undefined){
+					$element.remove();
+				} else {
+					$element.empty();
+					angular.forEach(collection, function(item){
+							
+						//Create new scope for every new item and load attributes to the scope
+						var childScope = $scope.$new();
+						angular.forEach(item, function(value, key){
+							childScope[key] = value;
+						});
+
+						$transclude(childScope, function(clone) {
+							$element.append(clone);
+						});
+			      	});
+				}
 		    });
 	    }	
 	}
